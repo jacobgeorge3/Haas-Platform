5e17f3876098dd8bcaae3fdbff3c8c9bdaa3ecf8
Zachary Chin <zachary@sixchins.com>
2021-11-02 02:48:19 -0500

    Created barebones flask file

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, send_from_directory
+from flask_cors import CORS, cross_origin
+import os
+
+app = Flask(__name__, static_folder='./build', static_url_path='')
+cors = CORS(app)
+
+@app.route('/api', methods=['GET'])
+@cross_origin()
+def index():
+  return "success!"
+
+@app.route('/')
+@cross_origin()
+def serve():
+  return send_from_directory(app.static_folder, "index.html")
+
+if __name__ == '__main__':
+  app.run()
