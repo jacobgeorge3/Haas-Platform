ad03e7303dc6ebc1073c656a6b73a2a450e1251f
sampippen <samfpippen@utexas.edu>
2021-11-09 10:09:42 -0600

    Login Page
    Color scheme off but that's for later to worry about

--- haas-avocados/src/pages/Login.js
+++ haas-avocados/src/pages/Login.js
@@ -3,18 +3,42 @@
  * Temp for landing
  */
 
- import React from "react";
- import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import React from "react";
+// import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import {TextField} from '@material-ui/core';
+import { Button } from "/Users/sampippen/Documents/GitHub/EE461L_Final_Project/haas-avocados/src/components/button.style.js";
+import "/Users/sampippen/Documents/GitHub/EE461L_Final_Project/haas-avocados/src/styles/login.css";
+
+function Login(){
+    function clickHandler() {
+        console.log("click")
+    }
+
+    return (
+        <div>
+            <h1>Login</h1>
+            <div className='login-container'>
+                <div className='login-input'>
+                    <TextField className='textfield' variant="outlined" label="Username" focused/>
+                </div>
+                <div>
+                    <TextField className='textfield' variant="outlined" label="Password" focused />
+                </div>        
+                <div className='forgot'>
+                    <a href="/login">Forgot password</a>
+                </div>
+            </div>
+          
+            <div className='buttons-container'>
+                <Button onclick="clickHandler()">
+                    Log in
+                </Button>
+                <Button>
+                    Register
+                </Button>
+            </div>    
+        </div>
+    )
+}
  
- class Login extends React.Component {
- 
-   render() {
-     return (
-       <>
-         <h1>Login</h1>
-       </>
-     )
-   }
- }
- 
- export default Login;
+export default Login;
