2514238c7f5689d050df0f9055afb5986425edb6
Zachary Chin <zachary@sixchins.com>
2021-11-10 15:31:10 -0600

    frick

--- backend/database.py
+++ backend/database.py
@@ -1,35 +1,101 @@
 import json
 import pymongo
 import certifi
+import uuid
 class DB:
-  def __init__(self, link, DBName, CollName):
+  def __init__(self, link, DBName, userCollectionName, projCollectionName, HWSetCollectionName):
     self.client = pymongo.MongoClient(link, tlsCAFile=certifi.where())
-    self.db = self.client[DBName]
-    self.usrCollection = self.db[CollName]
+    self.DB = self.client[DBName]
+    self.usrCollection = self.DB[userCollectionName]
+    self.projCollection = self.DB[projCollectionName]
+    self.usrCollection = self.DB[userCollectionName]
+    self.HWSetColletion = self.DB[HWSetCollectionName]
 
-  def addUser(self, formData):
-    formData = json.loads(formData)["formData"]
-    userDict = {
-      "_id":"",
-      "usr":formData["usr"],
-      "pass":formData["pass"]
-    }
-    if not self.usrCollection.find({"usr":userDict["usr"]}).count() > 0:
-      userDict["_id"] = "temp"
-      self.usrCollection.insert_one(userDict)
+  # ARGS: formDict. This is a python dictionary containing the user information.
+  # RETURNS: True on success, and False on duplicate username. We can add more error codes for password requirement failiures later.
+  def add_user(self, formDict):
+    if not self.usrCollection.find({"email":formDict["email"]}).limit(1).count() > 0:
+      id = str(uuid.uuid4())
+      while self.usrCollection.find({"_id": id}).limit(1).count() > 0:
+        id = str(uuid.uuid4())
+      formDict["_id"] = id
+      self.usrCollection.insert_one(formDict)
+      print("success",formDict)
+      #print("add_user: SUCCESS")
       return True
     else:
+      #print("add_user: USERNAME UNAVAILABLE")
+      print("failure", formDict)
+      return False
+    
+  def rem_user(self, userDict):
+    if self.usrCollection.find({"_id":userDict["_id"]}).limit(1).count() > 0:
+      self.usrCollection.remove({"_id":userDict["_id"]})
+      print("rem_user: SUCCESS")
+      return True
+    else:
+      print("rem_user: USER NOT FOUND")
       return False
 
-  def rem_user(self, user):
-    
+  def verify_user(self, userDict):
+    usrList = list(self.usrCollection.find({"email":userDict["email"]}).limit(1))
+    if len(usrList) == 0:
+      print("verify_user: USER NOT FOUND")
+      return False
+    else:
+      usr = usrList[0]
+      if usr["password"] == userDict["password"]:
+        return True
+      else:
+        return False
+
+  
+  
+  
+  
+  
+  
+  def create_project(self, userDict, projDict):
+    if not self.projCollection.find({"proj_id": projDict["proj_id"]}).limit(1).count() > 0:
+      self.usrCollection.update_one({"email" : userDict["email"]}, {"$push" : {"proj_list" : projDict["proj_id"]}})
+      projDict["user_list"].append(userDict["user_id"])
+      self.projCollection.insert_one(projDict)
+    else:
+      print("create_project: ID TAKEN")
+      
+  
+  def join_existing_project(self, userDict, projDict):
+    if self.projCollection.update({"proj_id": projDict["proj_id"]}, {"$push" : {"user_list": userDict["user_id"]}})["nModified"] > 0:
+      self.usrCollection.update_one({"email" : userDict["email"]}, {"$push" : {"proj_list" : projDict["proj_id"]}})
+      #print("join_existing_project: PROJECT JOINED.")
+    else:
+      print("join_existing_project: PROJECT DOESNT EXISTS.")
+
+ 
+  def rem_project(self, projDict):
+    proj = self.projCollection.find({"proj_id": projDict["proj_id"]}, {"user_list": 1}).limit(1)
+    if proj.count() > 0:
+      for user_id in proj[0]["user_list"]:
+        self.usrCollection.update({"user_id": user_id}, {"$pull": {"proj_list": projDict["proj_id"]}})
+    else:
+      print("rem_project: PROJECT NO LONGER EXISTS.")
+      return False
+  
+  def get_user_project(self, userDict):
+    usr_projs = list(self.usrCollection.find({"email" : userDict["email"]}, {"proj_list": 1}).limit(1))[0]
+    return usr_projs["proj_list"]
+  
+  
+  
+  
+  
+  
+  def init_hwSets(self):
     return 0
-  def verify_user():
+  def add_HW():
 
     return 0
-  def chng_user_pass():
-
+  def remove_HW():
     return 0
-  def chng_user_name():
-
+  def request_HW(self, num, HWSetDict):
     return 0
--- haas-avocados/src/pages/Login.js
+++ haas-avocados/src/pages/Login.js
@@ -3,51 +3,12 @@
  * Temp for landing
  */
 
-<<<<<<< Updated upstream
-import React from "react";
-// import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import {TextField} from '@material-ui/core';
-import { Button } from "/Users/sampippen/Documents/GitHub/EE461L_Final_Project/haas-avocados/src/components/button.style.js";
-import "/Users/sampippen/Documents/GitHub/EE461L_Final_Project/haas-avocados/src/styles/login.css";
-
-function Login(){
-    function clickHandler() {
-        console.log("click")
-    }
-
-    return (
-        <div>
-            <h1>Login</h1>
-            <div className='login-container'>
-                <div className='login-input'>
-                    <TextField className='textfield' variant="outlined" label="Username" focused/>
-                </div>
-                <div>
-                    <TextField className='textfield' variant="outlined" label="Password" focused />
-                </div>        
-                <div className='forgot'>
-                    <a href="/login">Forgot password</a>
-                </div>
-            </div>
-          
-            <div className='buttons-container'>
-                <Button onclick="clickHandler()">
-                    Log in
-                </Button>
-                <Button>
-                    Register
-                </Button>
-            </div>    
-        </div>
-    )
-}
-=======
  import React from "react";
  import {useState} from 'react';
  import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
  import {TextField} from '@material-ui/core';
  import Typography from "@mui/material/Typography";
- import "/Users/sampippen/Documents/GitHub/EE461L_Final_Project/haas-avocados/src/styles/login.css";
+ import "../styles/login.css";
 
  function Login(){
      return (
@@ -91,6 +52,5 @@ function Login(){
        </div>
      )
  }
->>>>>>> Stashed changes
  
 export default Login;
