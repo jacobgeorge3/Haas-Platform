4a6e0c686be083e9544de023d4fd2d1fb7f19de0
Ken Nguyen <55063699+Kenorthuy@users.noreply.github.com>
2021-11-12 23:55:50 -0600

    ok uhhh i guess its's done

--- haas-avocados/src/components/editproject.js
+++ haas-avocados/src/components/editproject.js
@@ -1,214 +1,327 @@
 /**
- * edit project component 
+ * edit project component
  * a simple form that'll create or edit a project
  * receives data from row click(edit) or an indicator that a row wasn't clicked(add)
  */
 
 import { Link } from "react-router-dom";
-import { TextField } from '@mui/material'
-import React, {useState} from 'react'
-import {Button} from '../components/button.style';
-import Auth from '../Auth'
+import { TextField } from "@mui/material";
+import React, { useState, useEffect } from "react";
+import { Button } from "../components/button.style";
+import Auth from "../Auth";
 
 import {
-    TextFieldWrapper,
-    FieldDesc,
-  } from "./dashboardcomponents/projectquickadd/projectquickadd.style";
+  TextFieldWrapper,
+  FieldDesc,
+} from "./dashboardcomponents/projectquickadd/projectquickadd.style";
+import { integerPropType } from "@mui/utils";
 const EditProject = (props) => {
-    const [pName, setName] = useState("");
-    const [pDesc, setDesc] = useState("");
-    const [h1, setH1] = useState(0);
-    const [h2, setH2] = useState(0);
+  const [pName, setName] = useState("");
+  const [pDesc, setDesc] = useState("");
+  const [h1Checkout, setH1Checkout] = useState(0);
+  const [h1Checkin, setH1Checkin] = useState(0);
+  const [h2Checkout, setH2Checkout] = useState(0);
+  const [h2Checkin, setH2Checkin] = useState(0);
+  const [datahw1, setDataHW1] = useState({});
+  const [datahw2, setDataHW2] = useState({});
+  // useEffect that will grab all hwsets a user is associated with
+  useEffect(() => {
+    Auth.get("/hw/get", { name: "hwset1" }).then((data) =>
+      setDataHW1(data["hwset"])
+    );
+    Auth.get("/hw/get", { name: "hwset2" }).then((data) =>
+      setDataHW2(data["hwset"])
+    );
+    setName(props.name);
+  }, []);
 
-    const hardwareDisplayStyle = {
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        flexDirection: 'row',
-        gap: '5px',
-        marginTop: '5px',
+  // useEffect(() => {
+  //     Auth.get('/hw/get').then(data => setData(data));
+  //     Object.keys(data).forEach(key => arr.push({name: key, value: data[key]}))
+  // },[]);
+
+  const hardwareDisplayStyle = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "row",
+    gap: "5px",
+    marginTop: "5px",
+  };
+
+  const hardwareInputStyle = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "column",
+    gap: "5px",
+    marginBottom: "16px",
+  };
+
+  const checkoutH1 = (e) => {
+    let amount = Number(h1Checkout);
+    try {
+      let amount = Number(h1Checkout);
+    } catch {
+      return;
+    }
+
+    const x = {
+      name: pName,
+      hwset: "hwset1",
+      amount: amount,
     };
 
-    const hardwareInputStyle = {
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        flexDirection: 'column',
-        gap: '5px',
-        marginBottom: '16px',
+    console.log(pName);
+
+    Auth.post("/hw/checkout", x).then((data) => {
+      console.log(JSON.stringify(data));
+      if (data["status"] == 200) {
+        console.log("success");
+      }
+      console.log("success again");
+    });
+  };
+
+  const checkoutH2 = (e) => {
+    try {
+      h2Checkout = Number(h2Checkout);
+    } catch {
+      return;
+    }
+
+    const x = {
+      name: pName,
+      hwset: "hwset2",
+      amount: h2Checkout,
     };
 
-    const checkout = () => {
-        console.log("checking out")
+    Auth.post("/hw/checkout", x).then((data) => {
+      console.log(JSON.stringify(data));
+      if (data["status"] == 200) {
+        console.log("success");
+      }
+      console.log("success again");
+    });
+  };
+
+  const checkinH1 = (e) => {
+    try {
+      h1Checkin = Number(h1Checkin);
+    } catch {
+      return;
     }
 
-//     const submit = (e) => {
-// 		Auth.login(userEmail, userPassword)
-// 			.then(data => {
-// 				console.log(JSON.stringify(data));
-// 				if (data['status'] == 200) {
-// 					updateApp();
-// 				}
-// 				setMsg(data['msg']);
-// 			})
-//   }
+    const x = {
+      name: pName,
+      hwset: "hwset1",
+      amount: h1Checkin,
+    };
 
-    const checkin = () => {
-        console.log("checking in")
+    Auth.post("/hw/checkin", x).then((data) => {
+      console.log(JSON.stringify(data));
+      if (data["status"] == 200) {
+        console.log("success");
+      }
+      console.log("success again");
+    });
+  };
+
+  const checkinH2 = (e) => {
+    let amount = Number(h2Checkin);
+
+    try {
+      let amount = Number(h2Checkin);
+    } catch {
+      return;
     }
 
-    function makeDict(){
-        const x =  {
-            'name':pName, 
-            'description':pDesc,
-            'hw1':parseInt(h1),
-            'hw2':parseInt(h2),
-            'user_list':[]
-        };
+    const x = {
+      name: pName,
+      hwset: "hwset2",
+      amount: amount,
+    };
 
-        Auth.post("/project/create", x).then(data => console.log(data));
-        //Function to post a new project given the input from the text fields
-    }
+    Auth.post("/hw/checkin", x).then((data) => {
+      console.log(JSON.stringify(data));
+      if (data["status"] == 200) {
+        console.log("success");
+      }
+      console.log("success again");
+    });
+  };
 
-    function saveChanges(){
-        //Code to change hardware sets will go here
-        //It would be awesome if we could also save changes to descriptions
-    }
+  // function makeDict(){
+  //     const x =  {
+  //         'name':pName,
+  //         'description':pDesc,
+  //         'state':0,
+  //         'hw1':parseInt(h1),
+  //         'hw2':parseInt(h2),
+  //         'user_list':[]
+  //     };
 
-    //Return a box with textfields for name, description, and each hardware set
-    //If editing a project have a save button that saves changes
-    //If adding a project have a create button that creates a new project
-    return (
-        <>
-          <div style={{padding: "10px"}}>
-            <TextFieldWrapper>
-                <TextField 
-                    variant="outlined" 
-                    color="primary" 
-                    focused multiline label="Project Name" 
-                    rows="1" 
-                    maxrows="6" 
-                    onChange={(e) => {setName(e.target.value)}} 
-                    defaultValue={props.name} 
-                />
-            </TextFieldWrapper>
-            <TextFieldWrapper>
-                <TextField 
-                    variant="outlined" 
-                    color="primary" 
-                    focused multiline label="Project Description" 
-                    rows="6" 
-                    maxrows="6" 
-                    onChange={(e) => {setDesc(e.target.value)}}
-                    defaultValue={props.description}
-                />
-            </TextFieldWrapper>
+  //     Auth.post("/project/create", x).then(data => console.log(data));
+  //     //Function to post a new project given the input from the text fields
+  // }
 
+  function saveChanges() {
+    //Code to change hardware sets will go here
+    //It would be awesome if we could also save changes to descriptions
+  }
 
-            {props.name == "" ? 
-                <Link to="/projects">
-                    <Button onClick={() => {console.log(makeDict())}}>Create</Button> 
-                </Link>
-                :  
-                <Link to="/projects">
-                    <Button onClick={() => {console.log("save Changes")}}>Save</Button>
-                </Link>
-            
-            }
+  //Return a box with textfields for name, description, and each hardware set
+  //If editing a project have a save button that saves changes
+  //If adding a project have a create button that creates a new project
+  return (
+    <>
+      <div style={{ padding: "10px" }}>
+        <TextFieldWrapper>
+          <TextField
+            variant="outlined"
+            color="primary"
+            focused
+            multiline
+            label="Project Name"
+            rows="1"
+            maxrows="6"
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+            defaultValue={props.name}
+          />
+        </TextFieldWrapper>
+        <TextFieldWrapper>
+          <TextField
+            variant="outlined"
+            color="primary"
+            focused
+            multiline
+            label="Project Description"
+            rows="6"
+            maxrows="6"
+            onChange={(e) => {
+              setDesc(e.target.value);
+            }}
+            defaultValue={props.description}
+          />
+        </TextFieldWrapper>
 
+        {props.name == "" ? (
+          <Link to="/projects">
+            <Button /*onClick={() => {console.log(makeDict())}}*/>
+              Create
+            </Button>
+          </Link>
+        ) : (
+          <Link to="/projects">
+            <Button
+              onClick={() => {
+                console.log("save Changes");
+              }}
+            >
+              Save
+            </Button>
+          </Link>
+        )}
 
-            {/*capacity and resources are hard coded for now*/}
-            <div style={hardwareDisplayStyle}>
-                <FieldDesc>HWSet 1</FieldDesc>
-                <FieldDesc>Capacity: 10</FieldDesc>
-                <FieldDesc>Available: 5</FieldDesc>
-            </div>
+        {/*capacity and resources are hard coded for now*/}
+        <div style={hardwareDisplayStyle}>
+          <FieldDesc>HWSet 1</FieldDesc>
+          <FieldDesc>{"Capacity: " + datahw1["capacity"]}</FieldDesc>
+          <FieldDesc>{"Available: " + datahw1["available"]}</FieldDesc>
+        </div>
 
-            <div style={hardwareInputStyle}>
-                <div style={hardwareDisplayStyle}>
-                    {/*checkin and checkout are not correct*/}
-                    <TextField 
-                        variant="outlined" 
-                        color="primary" 
-                        focused multiline label="Checkout Resources" 
-                        rows="1" 
-                        maxrows="1"  
-                        onChange={(e) => {setH1(e.target.value)}} 
-                        focused 
-                        defaultValue={props.hw1}
-                    />
-                    <Button onClick={checkout}>Checkout</Button> 
-                </div>
-                
-                <div style={hardwareDisplayStyle}>
-                    <TextField 
-                        variant="outlined" 
-                        color="primary" 
-                        focused multiline label="Checkin Resources" 
-                        rows="1" 
-                        maxrows="1"  
-                        onChange={(e) => {setH1(e.target.value)}} 
-                        focused 
-                        defaultValue={props.hw1}
-                    />
-                    <Button onClick={checkin}>Checkin</Button> 
-                </div>
-            </div>
-            
+        <div style={hardwareInputStyle}>
+          <div style={hardwareDisplayStyle}>
+            {/*checkin and checkout are not correct*/}
+            <TextField
+              variant="outlined"
+              color="primary"
+              focused
+              multiline
+              label="Checkout Resources"
+              rows="1"
+              maxrows="1"
+              onChange={(e) => {
+                setH1Checkout(e.target.value);
+              }}
+              focused
+              defaultValue={props.hw1}
+            />
+            <Button onClick={checkoutH1}>Checkout</Button>
+          </div>
 
+          <div style={hardwareDisplayStyle}>
+            <TextField
+              variant="outlined"
+              color="primary"
+              focused
+              multiline
+              label="Checkin Resources"
+              rows="1"
+              maxrows="1"
+              onChange={(e) => {
+                setH1Checkin(e.target.value);
+              }}
+              focused
+              defaultValue={props.hw1}
+            />
+            <Button onClick={checkinH1}>Checkin</Button>
+          </div>
+        </div>
 
-            {/*capacity and resources are hard coded for now*/}
-            <div style={hardwareDisplayStyle}>
-                <FieldDesc>HWSet 2</FieldDesc>
-                <FieldDesc>Capacity: 5</FieldDesc>
-                <FieldDesc>Available: 1</FieldDesc>
-            </div>
+        {/*capacity and resources are hard coded for now*/}
+        <div style={hardwareDisplayStyle}>
+          <FieldDesc>HWSet 2</FieldDesc>
+          <FieldDesc>{"Capacity: " + datahw2["capacity"]}</FieldDesc>
+          <FieldDesc>{"Available: " + datahw2["available"]}</FieldDesc>
+        </div>
 
-            <div style={hardwareInputStyle}>
-                <div style={hardwareDisplayStyle}>
-                    {/*checkin and checkout are not correct*/}
-                    <TextField 
-                        variant="outlined" 
-                        color="primary" 
-                        focused multiline label="Checkout Resources" 
-                        rows="1" 
-                        maxrows="1"  
-                        onChange={(e) => {setH1(e.target.value)}} 
-                        focused 
-                        defaultValue={props.hw1}
-                    />
-                    <Button onClick={checkout}>Checkout</Button> 
-                </div>
+        <div style={hardwareInputStyle}>
+          <div style={hardwareDisplayStyle}>
+            {/*checkin and checkout are not correct*/}
+            <TextField
+              variant="outlined"
+              color="primary"
+              focused
+              multiline
+              label="Checkout Resources"
+              rows="1"
+              maxrows="1"
+              onChange={(e) => {
+                setH2Checkout(e.target.value);
+              }}
+              focused
+              defaultValue={props.hw1}
+            />
+            <Button onClick={checkoutH2}>Checkout</Button>
+          </div>
 
-                <div style={hardwareDisplayStyle}>
-                    <TextField 
-                        variant="outlined" 
-                        color="primary" 
-                        focused multiline label="Checkin Resources" 
-                        rows="1" 
-                        maxrows="1"  
-                        onChange={(e) => {setH1(e.target.value)}} 
-                        focused 
-                        defaultValue={props.hw1}
-                    />
-                    <Button onClick={checkin}>Checkin</Button> 
-                </div>
-            </div>
-            
+          <div style={hardwareDisplayStyle}>
+            <TextField
+              variant="outlined"
+              color="primary"
+              focused
+              multiline
+              label="Checkin Resources"
+              rows="1"
+              maxrows="1"
+              onChange={(e) => {
+                setH2Checkin(e.target.value);
+              }}
+              focused
+              defaultValue={props.hw1}
+            />
+            <Button onClick={checkinH2}>Checkin</Button>
+          </div>
+        </div>
 
+        <p>{props.name}</p>
 
-            {/* {props.name == "" ? 
-                <Button onClick={() => {console.log(makeDict())}}>Create</Button> 
-                :  
-                <Link to="/projects">
-                    <Button onClick={() => {console.log("save Changes")}}>Save</Button>
-                </Link>
-            
-            } */}
-              
-          </div>         
-        </>
-    )
-}
+      </div>
+    </>
+  );
+};
 
 export default EditProject;
